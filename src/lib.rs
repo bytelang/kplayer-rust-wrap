2befe508f2bd8c45b93861bed49218733d3504da
karaler <karaler@bytelang.com>
2021-12-13 16:17:37 +0800

    feat: add export macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
 pub mod kplayer;
+
+#[macro_export]
+macro_rules! export {
+    ($fmt: expr) => {
+        #[no_mangle]
+        pub extern "C" fn Initialization() -> i32 {
+            kplayer::export_plugin(Box::new($fmt {}));
+            0
+        }
+    };
+}
